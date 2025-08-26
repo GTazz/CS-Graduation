9a1aaab3454d741154d4b274766224f69061af6c
GTazz <gabriel.oliveira.tazz@gmail.com>
2025-08-25 21:30:34 -0300

    add Java project to do later

--- 2-Semestre/1-Algoritimo/2-Aula/demo/src/main/java/com/example/Main.java
+++ 2-Semestre/1-Algoritimo/2-Aula/demo/src/main/java/com/example/Main.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int hours_worked;
+        double price_hour;
+
+        System.out.println("\nDigite as horas trabalhadas: ");
+        hours_worked = sc.nextInt();
+
+        System.out.println("\nDigite o valor da hora trabalhada: ");
+        price_hour = sc.nextDouble();
+
+        
     }
 }
