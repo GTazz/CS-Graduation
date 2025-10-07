0f69421fbc89a711371af87de9fe2352826f3ba9
GTazz <gabriel.oliveira.tazz@gmail.com>
2025-10-07 04:15:57 -0300

    study: java homework, working with string and array manipulation, its like Python, but depressing and humiliating

--- 2-Semestre/1-Algoritimo/4-Atividades/1-lerImprimirNum/src/App.java
+++ 2-Semestre/1-Algoritimo/4-Atividades/1-lerImprimirNum/src/App.java
@@ -1,45 +1,51 @@
 import java.util.Scanner;
+import java.util.Collections;
 
 public class App {
+    public static String _repeat(String str, int times) {
+        return String.join("", Collections.nCopies(times, str));
+    }
+
     static Scanner sc = new Scanner(System.in);
 
-    // ==================================== ATIVIDADE 1 ====================================
+    // ================ ATIVIDADE 1 ================
     public static void atividade1() {
         // Declaração de uma variável inteira chamada 'num'
         int num;
-    
+
         // Lê um número inteiro da entrada do usuário usando o objeto 'sc' (Scanner)
         System.out.println("ATIVIDADE 1\n");
         System.out.print("Número: ");
         num = sc.nextInt();
-        
+
         // Loop externo que itera de 1 até o valor de 'num' (inclusive)
         for (int i = 1; i <= num; i++) {
             // Imprime uma nova linha para começar uma nova sequência
             System.out.println("");
-            
+
             // Loop interno que itera de 1 até o valor atual de 'i' (inclusive)
             for (int i2 = 1; i2 <= i; i2++) {
                 // Imprime o valor de 'i2' seguido de um espaço, sem quebrar a linha
                 System.out.print(i2 + " ");
             }
         }
-        
-        // Imprime uma nova linha ao final para garantir que o cursor fique na linha seguinte
+
+        // Imprime uma nova linha ao final para garantir que o cursor fique na linha
+        // seguinte
         System.out.println("\n");
     }
-    
-    // ==================================== ATIVIDADE 2A ====================================
+
+    // =============== ATIVIDADE 2A ===============
     public static void atividade2a() {
         // Declaração de uma variável inteira chamada 'num'
         int num;
-    
+
         // Lê um número inteiro da entrada do usuário usando o objeto 'sc' (Scanner)
         System.out.println("ATIVIDADE 2B\n");
         System.out.print("Largura da bandeira: ");
         num = sc.nextInt();
-        
-        String[][] texts = new String[(num/2)+1][2];
+
+        String[][] texts = new String[(num / 2) + 1][2];
 
         texts[0][0] = "";
         texts[0][1] = "";
@@ -48,43 +54,54 @@ public class App {
         }
 
         int index = 0;
-        for (int i = num-1; i > 1; i=i-2) {
+        for (int i = num - 1; i > 1; i = i - 2) {
             index++;
-            texts[index][0] = texts[index-1][0] + "  ";
-            texts[index][1] = texts[index-1][1].substring(0, texts[index-1][1].length()-4);
+            texts[index][0] = texts[index - 1][0] + "  ";
+            texts[index][1] = texts[index - 1][1].substring(0, texts[index - 1][1].length() - 4);
         }
 
         System.out.println("");
         for (int i = index; i > 0; i--) {
             System.out.println(texts[i][0] + texts[i][1]);
-        } 
+        }
         for (int i = 0; i <= index; i++) {
             System.out.println(texts[i][0] + texts[i][1]);
         }
         System.out.println("");
     }
 
-    // ==================================== ATIVIDADE 2B ====================================    
+    // ================ ATIVIDADE 2B ================
     public static void atividade2b() {
         int num;
-    
+
         System.out.println("ATIVIDADE 2B\n");
         System.out.print("Número: ");
         num = sc.nextInt();
-
         
-        for (int i = num; i >= 1; i--) {
-            System.out.println("");
-            
-            for (int i2 = 1; i2 <= i; i2++) {
-                System.out.print("* ");
-            }
+        String characters = _repeat("  ", num - 1) + _repeat("* ", num);
+        
+        System.out.println("");
+        for (int i = 0; i < num * 2; i = i + 2) {
+            System.out.print(characters.substring(i, num * 2 - 2));
+            System.out.println(characters.substring(num * 2 - 2, num * 2 + i));
         }
+
+        System.out.println("");
+    }
+
+    // ================ ATIVIDADE 2C ================
+    public static void atividade2c() {
+        int num;
+
+        System.out.println("ATIVIDADE 2B\n");
+        System.out.print("Número: ");
+        num = sc.nextInt();
         
-        System.out.println("\n");
+        // Fiz assim só pra confundir mesmo kkkkkkkk
+        System.out.print(_repeat("\n" + _repeat("* ", num), num) + "\n\n");
     }
     
-    // ==================================== ATIVIDADE 3 ====================================    
+    // ================ ATIVIDADE 3 ================
     public static void atividade3() {
         double nota;
         int alunos;
@@ -95,56 +112,56 @@ public class App {
         String alunoMaior = "";
         String alunoMenor = "";
 
-        System.out.println("São quantos alunos? ");
+        System.out.println("ATIVIDADE 3\n");
+        System.out.print("São quantos alunos? ");
         alunos = sc.nextInt();
         sc.nextLine();
 
-        for (int i = 0; i < alunos; i++){
-        System.out.println("Nome do aluno:");
-        String nome = sc.nextLine();
+        for (int i = 0; i < alunos; i++) {
+            System.out.println("Nome do aluno:");
+            String nome = sc.nextLine();
 
-        System.out.println("Nota do aluno:");
-        nota = sc.nextInt();
-        sc.nextLine();
+            System.out.println("Nota do aluno:");
+            nota = sc.nextInt();
+            sc.nextLine();
 
-        somar += nota;
+            somar += nota;
 
-        if (i == 0) {
+            if (i == 0) {
 
-            maiorNota = nota;
-            menorNota = nota;
-            alunoMaior = nome;
-            alunoMenor = nome;
-        } else{
-            if (nota > maiorNota){
                 maiorNota = nota;
-                alunoMaior = nome;
-            }
-
-            if (nota <menorNota) {
                 menorNota = nota;
+                alunoMaior = nome;
                 alunoMenor = nome;
-                
+            } else {
+                if (nota > maiorNota) {
+                    maiorNota = nota;
+                    alunoMaior = nome;
+                }
+
+                if (nota < menorNota) {
+                    menorNota = nota;
+                    alunoMenor = nome;
+
+                }
             }
-        }
 
         }
 
-        double media = somar/alunos;
+        double media = somar / alunos;
 
         System.out.println("A média da turma foi: " + media + " de um total de " + alunos + " alunos.");
         System.out.println();
         System.out.println("O aluno com a maior nota foi o " + alunoMaior + " com uma nota de " + maiorNota);
         System.out.println();
         System.out.println("O aluno com a menor nota foi o " + alunoMenor + " com uma nota de " + menorNota);
-
     }
 
     public static void main(String[] args) throws Exception {
         App.atividade1();
         App.atividade2a();
         App.atividade2b();
-        // App.atividade2c();
+        App.atividade2c();
         App.atividade3();
     }
 }
